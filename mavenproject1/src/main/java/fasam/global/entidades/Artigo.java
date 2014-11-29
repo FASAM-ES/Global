d1287b003bf840db84c6df0f3663200b7089759a
Aluno <Aluno@172.16.1.56>
2014-11-29 11:17:45 -0200

    Criando projeto teste

--- mavenproject1/src/main/java/fasam/global/entidades/Artigo.java
+++ mavenproject1/src/main/java/fasam/global/entidades/Artigo.java
@@ -10,4 +10,43 @@ package fasam.global.entidades;
  */
 public class Artigo {
     
+    int _codigo;
+    String _titulo;
+    String _descricao;
+    
+    public int GetCodigo(){
+        return _codigo;
+    }
+    
+    public void SetCodigo(int icodigo){
+        _codigo = icodigo;
+    }
+    
+    public String GetTitulo(){
+        return _titulo;
+    }
+    
+    public void SetTitulo(String stitulo){
+        _titulo = stitulo;
+    }
+ 
+    public String GetDescricao(){
+        return _descricao;
+    }
+    
+    public void SetDescricao(String sdescricao){
+        _descricao = sdescricao;
+    }
+    
+    public int inserir(){
+        return 0;
+    }
+    
+    public int excluir(){
+        return 0;
+    }
+    
+    public int atualizar(){
+        return 0;
+    }
 }
