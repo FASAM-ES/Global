e3f356efef271920c5b54828d3778d4872e6577d
Aluno <Aluno@172.16.1.53>
2014-11-29 11:30:26 -0200

    Incluindo classe tag

---
+++ mavenproject1/src/main/java/fasam/global/entidades/Tag.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fasam.global.entidades;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Tag {
+    String _nome;
+    String _descricao;
+}
