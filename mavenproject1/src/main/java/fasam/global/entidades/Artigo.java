5448ae1f3637e40ad2104c7f1b86af0a54be6593
Aluno <Aluno@172.16.1.53>
2014-11-29 10:43:28 -0200

    Incluindo classe artigo

---
+++ mavenproject1/src/main/java/fasam/global/entidades/Artigo.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fasam.global.entidades;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Artigo {
+    
+}
