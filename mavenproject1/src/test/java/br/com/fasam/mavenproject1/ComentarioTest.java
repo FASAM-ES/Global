4d35e8b70c424530f897f07745217b9441482fbe
ronisdesousa <ronisdesousa@gmail.com>
2014-11-29 14:55:28 -0200

    Criando projeto teste

--- mavenproject1/src/test/java/br/com/fasam/mavenproject1/ComentarioTest.java
+++ mavenproject1/src/test/java/br/com/fasam/mavenproject1/ComentarioTest.java
@@ -5,10 +5,6 @@
 package br.com.fasam.mavenproject1;
 
 import fasam.global.entidades.Comentario;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import junit.framework.TestCase;
 
 /**
@@ -19,27 +15,6 @@ public class ComentarioTest extends TestCase {
     
     public ComentarioTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
     
     public void testTituloComentario(){
         Comentario comentario = new Comentario(); 
