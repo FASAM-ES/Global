c94477012fc00fa07a6ac7a4318d898c5f7b004b
Aluno <Aluno@172.16.1.53>
2014-11-29 11:36:21 -0200

    Incluindo classe tag

--- mavenproject1/src/main/java/fasam/global/entidades/Tag.java
+++ mavenproject1/src/main/java/fasam/global/entidades/Tag.java
@@ -11,4 +11,21 @@ package fasam.global.entidades;
 public class Tag {
     String _nome;
     String _descricao;
+
+    public String getNome() {
+        return _nome;
+    }
+
+    public String getDescricao() {
+        return _descricao;
+    }
+
+    public void setNome(String _nome) {
+        this._nome = _nome;
+    }
+
+    public void setDescricao(String _descricao) {
+        this._descricao = _descricao;
+    }
+    
 }
