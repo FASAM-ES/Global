fd2774f17a0527698980d3c3f28332fa2c206356
Demetrius <demetriusab@gmail.com>
2014-12-06 17:20:10 -0200

    Documentação

--- mavenproject1/src/main/java/fasam/global/entidades/Artigo.java
+++ mavenproject1/src/main/java/fasam/global/entidades/Artigo.java
@@ -19,46 +19,91 @@ public final class Artigo {
     List<Comentario> comentarios;
     Usuario usuario;
     
+    /**
+     * Cria a instância do artigo pelo comentário
+     * @param comentario 
+     */
     public Artigo(Comentario comentario){
         addComentarios(comentario);
     }
     
+    /**
+     * Cria a instância do artigo pelo usuário
+     * @param value 
+     */
     public Artigo(Usuario value){
         this.usuario = value;
     }
-        
+    
+    /**
+     * Retorna o código do artigo
+     * @return 
+     */
     public int getCodigo(){
         return this.codigo;
     }
     
+    /**
+     * Informa o código do artigo
+     * @param value 
+     */
     public void setCodigo(int value){
         this.codigo = value;
     }
     
+    /**
+     * Retorna o titulo do artigo
+     * @return 
+     */
     public String getTitulo(){
         return this.titulo;
     }
     
+    /**
+     * Informa o titulo do artigo
+     * @param value 
+     */
     public void setTitulo(String value){
         this.titulo = value;
     }
  
+    /**
+     * Retorna a descrição do artigo
+     * @return 
+     */
     public String getDescricao(){
         return this.descricao;
     }
     
+    /**
+     * Informa a descrição do artigo
+     * @param value 
+     */
     public void setDescricao(String value){
         this.descricao = value;
     }
 
+    /**
+     * Retorna o comentário do artigo
+     * @param index
+     * @return 
+     */
     public Comentario getComentario(int index) {
         return comentarios.get(index);
     }
 
+    /**
+     * Retorna o usuário do artigo
+     * @return 
+     */
     public Usuario getUsuario() {
         return this.usuario;
     }
         
+    /**
+     * Adicionar um novo comentário para o artigo
+     * @param comentario 
+     */
     public void addComentarios(Comentario comentario) {
         if(this.comentarios == null) {
             this.comentarios = new ArrayList<Comentario>();
@@ -66,6 +111,10 @@ public final class Artigo {
         this.comentarios.add(comentario);
     }
     
+    /**
+     * Remove um comentário do artigo 
+     * @param comentario 
+     */
     public void remComentarios(Comentario comentario) {
         if(this.comentarios != null) {
             this.comentarios.remove(comentario);
