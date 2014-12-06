71e0f4b055dc40b72e8f2c06409aeff0400d6e9e
alsistema <alsistema@gmail.com>
2014-12-06 16:34:55 -0200

    Adicionando comentários.

--- mavenproject1/src/main/java/fasam/global/entidades/Categoria.java
+++ mavenproject1/src/main/java/fasam/global/entidades/Categoria.java
@@ -18,14 +18,27 @@ public class Categoria {
     private String descricao;
     private List<Artigo> artigos;
 
+    /***
+     * Recebe o nome da categoria.
+     * @param nome 
+     */
     public Categoria(String nome) {
         this.nome = nome;
     }
     
+    /***
+     * Recupera o artigo.
+     * @param i
+     * @return retorna o artigo.
+     */
     public Artigo getArtigo(Integer i){
         return artigos.get(i);
     }
     
+    /***
+     * Adiciona um novo artigo.
+     * @param artigo 
+     */
     public void addArtigo(Artigo artigo){
         if(this.artigos == null){
             this.artigos = new ArrayList<Artigo>();
@@ -33,24 +46,44 @@ public class Categoria {
         this.artigos.add(artigo);
     }
     
+    /***
+     * Remove o artigo
+     * @param artigo 
+     */
     public void remArtigo(Artigo artigo){
         if(this.artigos != null){
             this.artigos.remove(artigo);
         }
     }
     
+    /***
+     * Recupera o nome da categoria.
+     * @return 
+     */
     public String getNome() {
         return nome;
     }
    
+    /***
+     * Seta o nome da categoria.
+     * @param nome 
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
     
+    /***
+     * Recupera a descrição.
+     * @return 
+     */
     public String getDescricao() {
         return descricao;
     }
    
+   /***
+    * Seta a descrição.
+    * @param descricao 
+    */
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
